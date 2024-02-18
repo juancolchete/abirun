87f337b4ed3d725cea35c2d15b49e913b4a9c294
juancolchete <juanudkp@gmail.com>
2024-02-18 13:10:10 -0300

    fix: import trouble defualt button

--- src/components/AbiSolver.tsx
+++ src/components/AbiSolver.tsx
@@ -131,7 +131,7 @@ const AbiSolver = (props: any) => {
 
   return (
     <>
-      <ConfirmModal   />
+      <ConfirmModal show={showConfirmModal} handleClose={handleClose}   />
     <Container className="col-sm">
       {address?.length > 0 &&(
         <>
--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import {Button} from 'react-bootstrap';
+import {Modal} from 'react-bootstrap';
 
-const ConfirmModal = () => {
-  const show = true;
-  const handleClose = ()=>{
-    console.log("close")
-  }
+const ConfirmModal = ({show, handleClose}) => {
 
   return (
     <>
       <Modal show={show} onHide={handleClose}>
-      {/*   <Modal.Header closeButton> */}
-      {/*     <Modal.Title>Modal heading</Modal.Title> */}
-      {/*   </Modal.Header> */}
-      {/*   <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body> */}
-      {/*   <Modal.Footer> */}
-      {/*     <Button variant="secondary" onClick={handleClose}> */}
-      {/*       Close */}
-      {/*     </Button> */}
-      {/*     <Button variant="primary" onClick={handleClose}> */}
-      {/*       Save Changes */}
-      {/*     </Button> */}
-      {/*   </Modal.Footer> */}
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   );
 }
 
 export {ConfirmModal};
+
