df086f9636c428ebd575decb38b144709eb72441
juancolchete <juanudkp@gmail.com>
2024-02-18 12:45:17 -0300

    debug!: add any type to confirm modal

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-const ConfirmModal = ({show, handleClose}) => {
+const ConfirmModal = ({show, handleClose}:any) => {
 
   return (
     <>
