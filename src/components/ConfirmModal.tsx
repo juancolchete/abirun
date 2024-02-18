1445109bcbde068491ad23d27af25c7e0c5a6fd2
juancolchete <juanudkp@gmail.com>
2024-02-18 12:55:14 -0300

    debug!: comment whole component

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -10,20 +10,20 @@ const ConfirmModal = () => {
 
   return (
     <>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      {/* <Modal show={show} onHide={handleClose}> */}
+      {/*   <Modal.Header closeButton> */}
+      {/*     <Modal.Title>Modal heading</Modal.Title> */}
+      {/*   </Modal.Header> */}
+      {/*   <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body> */}
+      {/*   <Modal.Footer> */}
+      {/*     <Button variant="secondary" onClick={handleClose}> */}
+      {/*       Close */}
+      {/*     </Button> */}
+      {/*     <Button variant="primary" onClick={handleClose}> */}
+      {/*       Save Changes */}
+      {/*     </Button> */}
+      {/*   </Modal.Footer> */}
+      {/* </Modal> */}
     </>
   );
 }
