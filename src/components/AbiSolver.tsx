3ece074b5754b9f2ef1d3161447896bf1d50c248
juancolchete <juanudkp@gmail.com>
2024-02-18 11:47:13 -0300

    debug!: show confirm modal withou functions

--- src/components/AbiSolver.tsx
+++ src/components/AbiSolver.tsx
@@ -131,7 +131,7 @@ const AbiSolver = (props: any) => {
 
   return (
     <>
-      {/* <ConfirmModal show={showConfirmModal} handleClose={handleClose} handleShow={handleShow} /> */}
+      <ConfirmModal show={showConfirmModal} />
     <Container className="col-sm">
       {address?.length > 0 &&(
         <>
