e4cb185ef0905b7de56fbd354cf12aa839aa8369
juancolchete <juanudkp@gmail.com>
2024-02-18 13:05:07 -0300

    debug!: change import way

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import {Modal,Button}  from 'react-bootstrap';
 
 const ConfirmModal = () => {
   const [show, setShow] = useState(false);
